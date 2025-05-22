31e8b2ca3973975d4be6667c57ffecaad2392376
Amirul <amirulal73@gmail.com>
2025-05-22 23:58:59 +0800

    Added Register functinality part 1

---
+++ resources/js/Pages/Auth/Register.vue
@@ -0,0 +1,17 @@
+<script setup>
+import Container from "../../Components/Container.vue";
+import TextLink from "../../Components/TextLink.vue";
+import Title from "../../Components/Title.vue";
+</script>
+
+<template>
+    <Container>
+        <div class="mb-8 text-center">
+            <Title>Register a new account</Title>
+            <p>
+                Already have an account?
+                <TextLink routeName="home" label="Login" />
+            </p>
+        </div>
+    </Container>
+</template>
